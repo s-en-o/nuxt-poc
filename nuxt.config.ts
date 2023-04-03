43f6c25a8278ecb3242a340de446f173128afc60
--global <--global>
2023-04-03 23:35:19 +1200

    chore: setup config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+    telemetry: false,
+    typescript: { shim: false },
+    app: {
+        head: {
+            title: 'Nuxt PoC',
+            titleTemplate: '%s - Nuxt PoC',
+            link: [
+                {
+                    href: 'https://fonts.googleapis.com',
+                    rel: 'preconnect',
+                },
+                {
+                    href: 'https://fonts.gstatic.com',
+                    rel: 'preconnect',
+                    crossorigin: 'anonymous',
+                },
+                {
+                    href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap',
+                    rel: 'stylesheet',
+                },
+            ],
+        },
+    },
+    css: ['~/assets/sass/main.scss'],
+});
